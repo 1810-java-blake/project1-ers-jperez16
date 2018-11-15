87b5f53091a0ff5b6bb9bbed1c315eca1d52d0b4
Jose <Joe.Antonio.Perez@gmail.com>
2018-11-15 14:25:11 -0600

    reinitialize

--- Server-SIde/project1/src/main/java/com/revature/daos/admindao.java
+++
@@ -1,8 +0,0 @@
-package com.revature.daos;
-import com.revature.controllers.admin_controller;
-public interface admindao {
-	public admindao instance = new admin_controller();
-	
-	Boolean updateUser(String auth, int roleID, int userID);
-
-}
--- Server-SIde/project1/src/main/java/com/revature/daos/logoutdao.java
+++
@@ -1,7 +0,0 @@
-package com.revature.daos;
-import com.revature.controllers.logout_controller;;
-public interface logoutdao {
-	logoutdao instance = new logout_controller();
-	Boolean userLogOut(String auth);
-
-}
